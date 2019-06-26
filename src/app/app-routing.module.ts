8badd5ee5ab783eeb1a3813f9f36c7b621ab4f3f
Nigel Norman <Nigel.Norman@proplogix.com>
2019-06-26 17:30:26 -0400

    adding routing and components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from './home/home.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { EditUserComponent } from './edit-user/edit-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "home", component: HomeComponent},
+  { path: "users", component: UserListComponent, children: [
+    { path: "new"},
+    { path: ":id", component: EditUserComponent}
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,16 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { BusinessListComponent } from './business-list/business-list.component';
+import { EditBusinessComponent } from './edit-business/edit-business.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { EditUserComponent } from './edit-user/edit-user.component';
+import { HomeComponent } from './home/home.component';
+import { NewUserComponent } from './new-user/new-user.component';
+import { NewBusinessComponent } from './new-business/new-business.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, BusinessListComponent, EditBusinessComponent, UserListComponent, EditUserComponent, HomeComponent, NewUserComponent, NewBusinessComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
